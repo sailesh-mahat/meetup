7abfb6ecbe57a41c2559695dae39326ec5ade8a0
sailesh-mahat <sailesharsenal@gmail.com>
2019-11-14 16:08:52 +0100

    4.3: added scatter chart

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { WarningAlert } from './Alert'
 import { getEvents } from './api';
-import logo from './images/logo.png'
+import logo from './images/logo.png';
+import moment from 'moment';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';
 
 
 
@@ -36,9 +40,31 @@ class App extends Component {
   else {
     getEvents(this.state.lat, this.state.lon, this.state.page).then(response => this.setState({ events: response }));
   }
-
-
 }
+
+countEventsOnADate = (date) => {
+    let count = 0;
+    for (let i = 0; i < this.state.events.length; i += 1) {
+      if (this.state.events[i].local_date === date) {
+        count += 1;
+      }
+    }
+    return count;
+  }
+
+  getData = () => {
+    const next7Days = []; // Create empty array for the next 7 days
+    const currentDate = moment(); // Today
+    // Loop 7 times for next 7 days
+    for (let i = 0; i < 7; i += 1) {
+      currentDate.add(1, 'days'); // Add 1 day to current date, currentDate changes
+      const dateString = currentDate.format('YYYY-MM-DD'); // Format the date
+      // Use the countEventsOnADate function to count #events on this date
+      const count = this.countEventsOnADate(dateString);
+      next7Days.push({ date: dateString, number: count }); // Add this date and number to the list
+    }
+    return next7Days;
+  }
 
   render() {
     return (
@@ -47,6 +73,19 @@ class App extends Component {
       <CitySearch updateEvents={this.updateEvents} />
       <NumberOfEvents updateEvents={this.updateEvents} numberOfEvents={this.state.events.length} lat={this.state.lat} lon={this.state.lon} />
       <WarningAlert text={this.state.warningText} />
+      <ResponsiveContainer height={400}>
+        <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="date" name="date" />
+            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+      </ResponsiveContainer>
       <EventList events={this.state.events} />
       </div>
     );
